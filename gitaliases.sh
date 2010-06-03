25b080925e6fb8e469ff328a3e2218c6438e3b40
Michel Kraemer <michel@undercouch.de>
2010-06-03 14:18:46 +0200

    Reformatted gitaliases.sh

--- gitaliases.sh
+++ gitaliases.sh
@@ -1,38 +1,18 @@
-# short g for git
+# aliases for common git commands
 alias g='git'
-
-# st for git status
 alias st='git status'
-
-# pull for git pull --rebase
-pull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
-
-# push for git push
 alias push='git push'
-
-# commit for git commit
 alias commit='git commit'
-
-# log for git log
 alias log='git log'
-
-# stash for git stash
 alias stash='git stash'
-
-# checkout for git checkout
 alias checkout='git checkout'
-
-# brach for git branch
 alias branch='git branch'
-
-# fetch for git fetch
 alias fetch='git fetch'
-
-# merge for git merge
 alias merge='git merge'
-
-# cherry-pick for git cherry-pick
 alias cherry-pick='git cherry-pick'
+
+# 'git pull --rebase' with a short log of the latest changes
+pull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
 
 # verbose add
 add () { git add -v $*; git status; }
@@ -40,8 +20,10 @@ add () { git add -v $*; git status; }
 # verbose reset
 reset () { git reset $*; git status; }
 
-# gadd for git add with git ls-files and grep, usage is like grep - example: gadd 'cs3d'
+# 'git add' with 'git ls-files' and grep
+# usage like grep - example: gadd 'readme.txt'
 gadd () { git ls-files -co --exclude-standard | grep $* | xargs git add -v; git status; }
 
+# 'git reset' with 'git ls-files' and grep
 greset () { git ls-files | grep $* | xargs git reset; git status; }
 
