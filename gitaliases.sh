eb77e6fd2bb3c96b39293066c24804cfe912c69e
Simon Zoellner <mail@simonzoellner.de>
2012-04-15 14:51:29 +0300

    "Prefixed all aliases with g and made them shorter"

--- gitaliases.sh
+++ gitaliases.sh
@@ -1,16 +1,16 @@
 # aliases for common git commands
 alias g='git'
-alias st='git status'
-alias push='git push'
-alias commit='git commit'
-alias log='git log'
-alias stash='git stash'
-alias checkout='git checkout'
-alias branch='git branch'
-alias fetch='git fetch'
-alias merge='git merge'
-alias cherry-pick='git cherry-pick'
-alias rebase='git rebase'
+alias gst='git status'
+alias gpush='git push'
+alias gcom='git commit'
+alias glog='git log'
+alias gsta='git stash'
+alias gche='git checkout'
+alias gbra='git branch'
+alias gfet='git fetch'
+alias gmer='git merge'
+alias gcherpi='git cherry-pick'
+alias greb='git rebase'
 
 # 'git pull --rebase' with a short log of the latest changes
 pull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
@@ -31,5 +31,7 @@ gadd () { git ls-files -co --exclude-standard | grep $* | xargs git add -v; git 
 # 'git reset' with 'git ls-files' and grep
 greset () { git ls-files | grep $* | xargs git reset; git status; }
 
+# Enable auto-completion for aliased 'git' command
+complete -o default -o nospace -F _git g
 # Enable auto-completion for aliased 'git' command
 complete -o default -o nospace -F _git g
