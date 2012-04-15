c25d7bbc370951e5b8f8dff72952887c3050d354
Simon Zoellner <mail@linutux.net>
2012-04-15 14:22:31 +0200

    Prefixed the missing commands with g, changed some and fixed the last line

--- gitaliases.sh
+++ gitaliases.sh
@@ -13,25 +13,23 @@ alias gcherpi='git cherry-pick'
 alias greb='git rebase'
 
 # 'git pull --rebase' with a short log of the latest changes
-pull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
+gpull () { local HEADHASH=`git describe --always --abbrev=40`; git pull --rebase $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
 
 # 'git pull --ff-only' with a short log of the latest changes
-ff () { local HEADHASH=`git describe --always --abbrev=40`; git pull --ff-only $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
+gff () { local HEADHASH=`git describe --always --abbrev=40`; git pull --ff-only $*; echo; PAGER='cat -B' git log --format="%C(yellow)%h %C(green)%an%C(reset): %s" $HEADHASH.. | sed -nr 's/([^:]+)\:/\1\t/;p'; }
 
 # verbose add
-add () { git add -v $*; git status; }
+gadd () { git add -v $*; git status; }
 
 # verbose reset
-reset () { git reset $*; git status; }
+greset () { git reset $*; git status; }
 
 # 'git add' with 'git ls-files' and grep
 # usage like grep - example: gadd 'readme.txt'
-gadd () { git ls-files -co --exclude-standard | grep $* | xargs git add -v; git status; }
+glsadd () { git ls-files -co --exclude-standard | grep $* | xargs git add -v; git status; }
 
 # 'git reset' with 'git ls-files' and grep
-greset () { git ls-files | grep $* | xargs git reset; git status; }
+glsreset () { git ls-files | grep $* | xargs git reset; git status; }
 
-# Enable auto-completion for aliased 'git' command
-complete -o default -o nospace -F _git g
 # Enable auto-completion for aliased 'git' command
 complete -o default -o nospace -F _git g
